449e5d7d8b743607983a14c4562ba5bfed3fae0d
lee <ce.li@thoughtworks.com>
2020-06-13 13:01:17 +0800

    homework of es6

--- src/practice1.js
+++ src/practice1.js
@@ -1,2 +1,16 @@
-const parseData
+const parseData = input => {
+    const result = [];
+    for (let data of input.data) {
+        const inner = {};
+        let number = 0;
+        for (let format of input.column) {
+            const label = Object.values(format);
+            label[1] = data[number];
+            inner[label[0]] = label[1];
+            number++;
+        }
+        result.push(inner)
+    }
+    return result;
+}
 export { parseData };
--- src/practice2.js
+++ src/practice2.js
@@ -1,2 +1,11 @@
-const inject
+const inject = (items, sections) => {
+    for (let i = sections.length - 1; i >= 0; i--) {
+        let item = sections[i];
+        let eachValue = Object.entries(item);
+        let value = eachValue[0][1];
+        let index = eachValue[1][1];
+        items.splice(index, 0, value);
+    }
+    return items;
+}
 export { inject };
